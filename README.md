31e5b384c76fe985eb5826d6130b215e6ab61460
Grant Hunter <gr@nt-hunter.ca>
2017-03-13 20:05:46 -0600

    opengl works ish

--- README.md
+++ README.md
@@ -2,9 +2,23 @@ Android Meetup 2017-03-15: Rust on Android
 ==========================================
 
 Using:
-=====
+======
+- rustup [https://rustup.rs/](https://rustup.rs/)
 - rust 1.15.1
 - cargo-apk
-- android_glue (https://github.com/tomaka/android-rs-glue)[https://github.com/tomaka/android-rs-glue]
+- android_glue [https://github.com/tomaka/android-rs-glue](https://github.com/tomaka/android-rs-glue)
+- cargo-script [https://github.com/DanielKeep/cargo-script](https://github.com/DanielKeep/cargo-script)
+
+
+
+Intro:
+======
+- using rustup: installing rust, toolchains, etc...
+- cargo and adding dependencies 
+- cargo script!!
+
+Rust on Android:
+================
+- android glue: library + build scripts
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,74 @@
 extern crate android_glue;
+extern crate gl;
+extern crate glutin;
+extern crate libc;
 
-
-use android_glue::{write_log, Event, add_sender};
+use android_glue::Event;
 
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::thread;
 
+fn do_ui() {
+    let window = glutin::WindowBuilder::new()
+        .with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGlEs, (3, 0)))
+        .build()
+        .unwrap();
+
+    unsafe {
+        window.make_current();
+        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
+    }
+
+    for event in window.wait_events() {
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+         window.swap_buffers();
+
+        match event {
+            glutin::Event::Closed => break,
+            _ => ()
+        }
+    }
+
+}
+
+
 
 fn main() {
-    write_log("Hello, world!");
+    android_glue::write_log("Hello, world!");
 
-    let (sender, receiver) : (Sender<Event>, Receiver<Event>) = mpsc::channel();
+    let (sender, receiver): (Sender<Event>, Receiver<Event>) = mpsc::channel();
 
-    add_sender(sender);
+    android_glue::add_sender(sender);
 
-    thread::spawn(move || {
+    loop {
+        let event = match receiver.recv() {
+            Ok(e) => e,
+            Err(_) => continue,
+        };
 
-        loop{
-            let event = receiver.recv();
-            println!("Event: {:?}", event);
+
+
+        match event {
+            Event::Start => {
+                println!("Starting app!!");
+            }
+            Event::InitWindow => {
+                thread::spawn(do_ui);
+            }
+            Event::Stop | Event::Pause => {
+                println!("Stop or Pause!!");
+            }
+            Event::TermWindow => {
+                println!("Window terminated");
+            }
+
+            _ => {}
         }
-        
-    });
+
+        println!("Event: {:?}", event);
+    }
 
 
 }
